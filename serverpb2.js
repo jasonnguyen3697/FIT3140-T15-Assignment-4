152594956bbff45fb25d3cccee487b3048db0ad1
Ming-Yiu <37524280+Ming-Yiu@users.noreply.github.com>
2018-05-06 16:27:00 +1000

    Update serverpb2.js

--- serverpb2.js
+++ serverpb2.js
@@ -185,8 +185,8 @@ server3001.post('/', function(request, response){
   var buf = messages.Transac.encode({
     from: request.body.client_from,
     to: request.body.client_to,
-    amount: request.body.client_amount,
-    description: request.body.client_description
+    amount: request.body.amount,
+    description: request.body.description
   });
   if (!chooseServer)
   {
